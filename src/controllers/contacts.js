c3581c463fc6017a7bb74eb4433b73e36d4586cc
TetianaChehrina <3909139@gmail.com>
2024-07-16 19:41:19 +0200

    hw2

---
+++ src/controllers/contacts.js
@@ -0,0 +1,35 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getAllContactsControler = async (req, res, next) => {
+  try {
+    const contacts = await getAllContacts();
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data: contacts,
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
+export const getContactByIdControler = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const contact = await getContactById(contactId);
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact Not found',
+      });
+      return;
+    }
+    res.status(200).json({
+      status: 200,
+      message: 'Succesfully found product with id {contactId}',
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ src/routers/contacts.js
@@ -0,0 +1,10 @@
+// import { Router } from 'express';
+// import {
+//   getAllContactsControler,
+//   getContactByIdControler,
+// } from '../controllers/contacts.js';
+
+// const router = Router();
+// app.get('/contacts', getAllContactsControler);
+// app.get('/contacts/:contactId', getContactByIdControler);
+// export default router;
