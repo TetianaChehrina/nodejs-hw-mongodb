636114cd14a04976a74ceb25f60c4698ce9e7bd7
TetianaChehrina <3909139@gmail.com>
2024-08-11 17:46:42 +0200

    services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -44,7 +44,7 @@ export const getContactById = async (contactId, userId) => {
 };
 
 export const createContact = async (contactData) => {
-  const contact = await ContactCollection.create(contactData);
+  const contact = new ContactCollection(contactData);
   return contact;
 };
 
