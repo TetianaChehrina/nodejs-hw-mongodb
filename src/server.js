a11eadeb86e56b468570158d16210a0817778239
TetianaChehrina <3909139@gmail.com>
2024-08-17 16:36:04 +0200

    openapi

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import dotenv from 'dotenv';
 import { router } from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 dotenv.config();
 
 const PORT = Number(env('PORT', '3000'));
@@ -27,6 +28,7 @@ export const setupServer = () => {
   app.use(notFoundHandler);
   app.use(errorHandler);
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs());
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
